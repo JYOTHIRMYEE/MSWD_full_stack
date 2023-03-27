20956ad09f25d9d5b79150678487357f87691856
JYOTHIRMYEE <129038271+JYOTHIRMYEE@users.noreply.github.com>
2023-03-27 13:41:59 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
-// ***********************************************************
-// This example support/index.js is processed and
-// loaded automatically before your test files.
-//
-// This is a great place to put global configuration and
-// behavior that modifies Cypress.
-//
-// You can change the location of this file or turn off
-// automatically serving support files with the
-// 'supportFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/configuration
-// ***********************************************************
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
-// Import commands.js using ES2015 syntax:
-import './commands'
+export const useField = (type) => {
+  const [value, setValue] = useState('')
 
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  return {
+    type,
+    value,
+    onChange
+  }
+}
+
+export const useResource = (baseUrl) => {
+  const [resources, setResources] = useState([])
+
+  useEffect(() => {
+    axios
+      .get(baseUrl)
+      .then(response => {
+        setResources(response.data)
+      })
+  }, [setResources, baseUrl])
+
+  const create = async newObject => {
+    const response = await axios.post(baseUrl, newObject)
+    setResources(resources.concat(response.data))
+  }
+
+  const service = {
+    create
+  }
+
+  return [
+    resources, service
+  ]
+}
